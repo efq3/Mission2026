c42d5dfd70053b323f6768a846e37f4a2a72e01a
youngsoo44 <qmzpeo@gnu.ac.kr>
2026-05-19 21:48:50 +0900

    모델 학습파일

---
+++ train.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    
+    # 모델 불러오기
+    model = YOLO("yolo11n-pose.pt")
+    
+    # workers=0 을 넣어서 멀티프로세싱 충돌 원천 차단
+    model.train(
+        # 하이퍼 파라미터
+        data=".../pose_dataset.yaml", 
+        epochs=50, 
+        imgsz=640, 
+        batch=8, 
+        device=0, 
+        workers=0 
+    )
