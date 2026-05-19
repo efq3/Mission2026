67b6d45bf9f8055cefb4d3381e74a94c1924a7cc
youngsoo44 <qmzpeo@gnu.ac.kr>
2026-05-19 22:14:21 +0900

    모델 학습파일

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     # workers=0 을 넣어서 멀티프로세싱 충돌 원천 차단
     model.train(
         # 하이퍼 파라미터
-        data=".../pose_dataset.yaml", 
+        data=".../pose_dataset.yaml", # 데이터셋 경로
         epochs=50, 
         imgsz=640, 
         batch=8, 
