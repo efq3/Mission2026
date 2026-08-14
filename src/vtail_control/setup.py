026dd1d6fa1d4dd4881912c74c188e2198cf9130
efq3 <nahyunjung04@gmail.com>
2026-08-14 23:31:50 +0900

    RaspberryPi5

--- src/vtail_control/setup.py
+++ src/vtail_control/setup.py
@@ -8,12 +8,17 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    package_data={'vtail_control': ['best.pt']},
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+        (os.path.join('share', package_name, 'models', 'target_board_car'), glob('models/target_board_car/*.*')),
+        (os.path.join('share', package_name, 'models', 'target_board_car', 'materials', 'textures'), glob('models/target_board_car/materials/textures/*')),
+        (os.path.join('share', package_name, 'models', 'target_board_tank'), glob('models/target_board_tank/*.*')),
+        (os.path.join('share', package_name, 'models', 'target_board_tank', 'materials', 'textures'), glob('models/target_board_tank/materials/textures/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -29,10 +34,13 @@ setup(
     entry_points={
         'console_scripts': [
             'guidance_node = vtail_control.guidance_node:main',
-            'visualize_node = vtail_control.visualize_node:main',
-            'target_marker_node = vtail_control.target_marker_node:main',
+            'visualize_node = vtail_control.rviz_f.visualize_node:main',
+            'target_marker_node = vtail_control.rviz_f.target_marker_node:main',
             'mode_node = vtail_control.mode_node:main',
             'yolo_node = vtail_control.yolo_node:main',
+            'qgc_streamer_node = vtail_control.qgc_streamer_node:main',
+            'gz_gst_bridge_node = vtail_control.gz_gst_bridge_node:main',
+            'python_gz_ros_bridge = vtail_control.python_gz_ros_bridge:main'
         ],
     },
 )
